67a51ecc8bc247892813a5c31104e60f289c16d7
Horace Velmont <dragond7@naver.com>
2020-02-03 20:42:56 +0900

    fix: config 수정

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
+  pathPrefix: '/horace-velmont',
+  title: 'Blog by Horace Velmont',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
@@ -25,7 +25,7 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'DaeHoon Kim (Velmont)',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
